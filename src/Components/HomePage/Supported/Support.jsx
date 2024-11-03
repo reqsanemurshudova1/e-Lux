8d3dc8d3fc2a5e19262c47b07f29a4d8814a63b4
Sehanexelilova <sehane.wqizi@gmail.com>
2024-11-03 21:27:35 +0400

    integrate backend api to payment methods

--- src/Components/HomePage/Supported/Support.jsx
+++ src/Components/HomePage/Supported/Support.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Support.css';
+import axios from "axios";
 
 const logos = [
   { src: './Assets/Zara.jpg', alt: 'Zara' },
@@ -11,15 +12,35 @@ const logos = [
 ];
 
 export default function Support() {
-  return (
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/api/partners');
+                setData(response.data?.data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
     <div className='support container'>
       <div className="support-title">
         We Are Supported By
       </div>
       <div className="support-logos">
-        {logos.map((logo, index) => (
+        {data.map((logo, index) => (
           <div className="card-logo" key={index}>
-            <img src={logo.src} alt={logo.alt} />
+            <img src={`http://e_lux_backend.test/storage/${logo.img}`} alt={logo.title}
+            />
           </div>
         ))}
       </div>
